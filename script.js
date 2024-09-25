89bb49b68d5bac1abad1e134f7bfc181bc964e14
SULE-L <lusulita@gmail.com>
2024-09-25 09:17:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+body {
+    font-family: 'Arial Rounded', sans-serif;
+    background-color: #f0f0f0;
+    color: #333;
+}
+table, td, th {
+    border: 1px solid white;
+}
+table {
+    border-collapse: collapse;
+    width: 100%;
+    margin-bottom: 20px;
+    background-color: white;
+}
+td {
+    text-align: center;
+}
+h2 {
+    text-align: left;
+}
+.right {
+    text-align: right;
+}
+@media print {
+    body {
+        width: 210mm;
+        height: 297mm;
+    }
+    button {
+        display: none;
+    }
+}
